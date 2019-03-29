c1b60048a8e844bbbdeaca58252be647fe3ff006
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2019-03-29 10:00:08 +0100

    Fixed board bg and started route vars

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Defining essential variables
+
+var routePlayer1 = ["TL0", "TL1", "TL2", "TL3", "TL4", "TL5", "TL6", "TL7", "TL8", "TL9", "TL10", "TL11", "TL12", "TL13",
+"TR0", "TR1", "TR2", "TR3", "TR4", "TR5", "TR6", "TR7", "TR8", "TR9", "TR10", "TR11", "TR12", "TR13",
+"BL0", "BL1", "BL2", "BL3", "BL4", "BL5", "BL6", "BL7", "BL8", "BL9", "BL10", "BL11", "BL12", "BL13",
+"BR0", "BR1", "BR2", "BR3", "BR4", "BR5", "BR6", "BR7", "BR8", "BR9", "BR10", "BR11", "BR12",
+"eL1", "eL2", "eL3", "eL4", "eL5", "eL6"];
+
+var routePlayer2 = ["TR0", "TR1", "TR2", "TR3", "TR4", "TR5", "TR6", "TR7", "TR8", "TR9", "TR10", "TR11", "TR12", "TR13",
+"BL0", "BL1", "BL2", "BL3", "BL4", "BL5", "BL6", "BL7", "BL8", "BL9", "BL10", "BL11", "BL12", "BL13",
+"BR0", "BR1", "BR2", "BR3", "BR4", "BR5", "BR6", "BR7", "BR8", "BR9", "BR10", "BR11", "BR12", "BR13",
+"TL0", "TL1", "TL2", "TL3", "TL4", "TL5", "TL6", "TL7", "TL8", "TL9", "TL10", "TL11", "TL12",
+"eT1", "eT2", "eT3", "eT4", "eT5", "eT6"];
+
+var routePlayer3 = [];
+
+var routePlayer4 = [];
+
+
+"TL0", "TL1", "TL2", "TL3", "TL4", "TL5", "TL6", "TL7", "TL8", "TL9", "TL10", "TL11", "TL12", "TL13",
+"TR0", "TR1", "TR2", "TR3", "TR4", "TR5", "TR6", "TR7", "TR8", "TR9", "TR10", "TR11", "TR12", "TR13",
+"BL0", "BL1", "BL2", "BL3", "BL4", "BL5", "BL6", "BL7", "BL8", "BL9", "BL10", "BL11", "BL12", "BL13",
+"BR0", "BR1", "BR2", "BR3", "BR4", "BR5", "BR6", "BR7", "BR8", "BR9", "BR10", "BR11", "BR12", "BR13",
+
+"eT1", "eT2", "eT3", "eT4", "eT5", "eT6"
+"eR1", "eR2", "eR3", "eR4", "eR5", "eR6"
+"eB1", "eB2", "eB3", "eB4", "eB5", "eB6"
+"eL1", "eL2", "eL3", "eL4", "eL5", "eL6"
