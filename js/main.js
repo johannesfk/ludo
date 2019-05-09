4c0e4aceddd816a7c560fb6ff928a2474f383cae
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2019-05-09 12:27:46 +0200

    moving pieces test

--- js/main.js
+++ js/main.js
@@ -21,13 +21,13 @@ var routePlayer2 = ["TR0", ".TR1", ".TR2", ".TR3", ".TR4", ".TR5", ".TR6", ".TR7
 var routePlayer3 = ["BR0", ".BR1", ".BR2", ".BR3", ".BR4", ".BR5", ".BR6", ".BR7", ".BR8", ".BR9", ".BR10", ".BR11", ".BR12", ".BR13",
 "BL0", ".BL1", ".BL2", ".BL3", ".BL4", ".BL5", ".BL6", ".BL7", ".BL8", ".BL9", ".BL10", ".BL11", ".BL12", ".BL13",
 "TL0", ".TL1", ".TL2", ".TL3", ".TL4", ".TL5", ".TL6", ".TL7", ".TL8", ".TL9", ".TL10", ".TL11", ".TL12", ".TL13",
-"TR0", ".TR1", ".TR2", ".TR3", ".TR4", ".TR5", ".TR6", ".TR7", ".TR8", ".TR9", ".TR10", ".TR11", ".TR12", ".TR12",
+"TR0", ".TR1", ".TR2", ".TR3", ".TR4", ".TR5", ".TR6", ".TR7", ".TR8", ".TR9", ".TR10", ".TR11", ".TR12",
 "eR1", ".eR2", ".eR3", ".eR4", ".eR5", ".eR6"];
 
 var routePlayer4 = ["BL0", ".BL1", ".BL2", ".BL3", ".BL4", ".BL5", ".BL6", ".BL7", ".BL8", ".BL9", ".BL10", ".BL11", ".BL12", ".BL13",
 "TL0", ".TL1", ".TL2", ".TL3", ".TL4", ".TL5", ".TL6", ".TL7", ".TL8", ".TL9", ".TL10", ".TL11", ".TL12", ".TL13",
 "TR0", ".TR1", ".TR2", ".TR3", ".TR4", ".TR5", ".TR6", ".TR7", ".TR8", ".TR9", ".TR10", ".TR11", ".TR12", ".TR13",
-"BR0", ".BR1", ".BR2", ".BR3", ".BR4", ".BR5", ".BR6", ".BR7", ".BR8", ".BR9", ".BR10", ".BR11", ".BR12", ".BR12",
+"BR0", ".BR1", ".BR2", ".BR3", ".BR4", ".BR5", ".BR6", ".BR7", ".BR8", ".BR9", ".BR10", ".BR11", ".BR12",
 "eB1", ".eB2", ".eB3", ".eB4", ".eB5", ".eB6"];
 
 // "TL0", "TL1", "TL2", "TL3", "TL4", "TL5", "TL6", "TL7", "TL8", "TL9", "TL10", "TL11", "TL12", "TL13",
@@ -83,6 +83,20 @@ if (gameRound == 0) {
 }
 
 function movePiece() {
-  // FIXME: the thing what moves the pieces
-  $( "#p1_1" ).appendTo( $("routePlayer1[gameRound]") );
+  $( "#p1_1" ).appendTo(routePlayer1[gameRound]);
+  $( "#p1_2" ).appendTo(1 + routePlayer1[gameRound]);
+  $( "#p1_3" ).appendTo(2 + routePlayer1[gameRound]);
+  $( "#p1_4" ).appendTo(3 + routePlayer1[gameRound]);
+  $( "#p2_1" ).appendTo(routePlayer2[gameRound]);
+  $( "#p2_2" ).appendTo(1 + routePlayer2[gameRound]);
+  $( "#p2_3" ).appendTo(2 + routePlayer2[gameRound]);
+  $( "#p2_4" ).appendTo(3 + routePlayer2[gameRound]);
+  $( "#p3_1" ).appendTo(routePlayer3[gameRound]);
+  $( "#p3_2" ).appendTo(1 + routePlayer3[gameRound]);
+  $( "#p3_3" ).appendTo(2 + routePlayer3[gameRound]);
+  $( "#p3_4" ).appendTo(3 + routePlayer3[gameRound]);
+  $( "#p4_1" ).appendTo(routePlayer4[gameRound]);
+  $( "#p4_2" ).appendTo(1 + routePlayer4[gameRound]);
+  $( "#p4_3" ).appendTo(2 + routePlayer4[gameRound]);
+  $( "#p4_4" ).appendTo(3 + routePlayer4[gameRound]);
 }
