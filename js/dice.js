52b6ee26ef3818eb9f8c707b99a904ce701a1af0
Johannes Farmer Knudsen <johannesfknudsen@gmail.com>
2019-05-14 12:48:00 +0200

    Update dice.js

--- js/dice.js
+++ js/dice.js
@@ -6,11 +6,11 @@ function rollDice() {
   die.innerHTML = currentDiceInt;
   diceOut.innerHTML = currentDiceInt;
   diceHits.unshift(currentDiceInt);
-  gameRound += 1;
-  printRoundStatus()
+  printRoundStatus();
   movePiece();
-
+  
   // Changes current player per round
+  gameRound += 1;
   if (currentPlayerID == 3) {
     currentPlayerID = 0;
   } else {
